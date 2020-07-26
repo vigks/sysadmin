3675eac018ecc28d3e7f2c9abf2bac413c663982
yosun <njnovice@gmai.com>
2020-07-26 17:47:36 +0800

    '初始化‘

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Element from 'element-ui'
+import store from './store'
+import router from './router'
+import './styles/element-variables.scss'
+import * as filters from './filters' // global filters
+
+Vue.config.productionTip = false
+
+Vue.use(Element)
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
