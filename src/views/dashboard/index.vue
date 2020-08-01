3f98c352f53eea7fdd6a79c40114fb7a326577d9
315501094@qq.com <315501094@qq.com>
2020-08-01 14:59:24 +0800

    'proxy'

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -9,3 +9,13 @@
         name: 'Dashboard',
     }
 </script>
+<style lang="scss" scoped>
+    .dashboard-container {
+        background-image: url("../../assets/images/dashboard/bg.png");
+        background-size: cover;
+        background-position: 0 0;
+        background-repeat: no-repeat no-repeat;
+        height: 100%;
+        width: 100%;
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    // devServer: {
-    //     proxy: {
-    //         '/renren-security': {
-    //             target:'http://47.92.173.68/',
-    //             ws:true,
-    //             changOrigin:true,
-    //             pathRewrite:{
-    //                 '^/renren-security':'/renren-security'
-    //             }
-    //         }
-    //     }
-    // }
-}
+    devServer: {
+        proxy: {
+            '*': {
+                target:'http://47.92.173.68/',
+                ws:true,
+                changOrigin:true,
+                pathRewrite:{
+                    // '^/renren-security':'/renren-security'
+                }
+            }
+        }
+    }
+};
